50671e456166dec1cf53d844d2b9879ab6f94b4b
moxarth-elastic <moxarth.rathod@elastic.co>
2022-04-03 21:31:04 +0530

    Updated the basic strcuture files

--- ees_microsoft_teams/__init__.py
+++ ees_microsoft_teams/__init__.py
@@ -3,7 +3,6 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
-"""This module provides the tooling to synchronize data
-    from Microsoft Teams to Elastic Enterprise Search.
-"""
+"""This module provides the tooling to synchronize
+data from Microsoft Teams to Enterprise Search."""
 __version__ = "0.1.0"
