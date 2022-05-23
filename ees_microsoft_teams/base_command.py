eb9a3336a069107bb7ac43d194e0248feccba81e
moxarth-elastic <moxarth.rathod@elastic.co>
2022-05-23 22:55:24 +0530

    Update the code as per the latest changes

--- ees_microsoft_teams/base_command.py
+++ ees_microsoft_teams/base_command.py
@@ -10,7 +10,10 @@
     objects and methods that will can be used by commands.
 """
 
+import csv
+import functools
 import logging
+import os
 
 try:
     from functools import cached_property
@@ -26,6 +29,7 @@ from .configuration import Configuration
 from .local_storage import LocalStorage
 from .microsoft_teams_calendars import MSTeamsCalendar
 from .msal_access_token import MSALAccessToken
+from .permission_sync_command import PermissionSyncCommand
 from .utils import get_schema_fields, split_documents_into_equal_chunks
 
 
@@ -55,6 +59,8 @@ class BaseCommand:
         logger.setLevel(log_level)
 
         handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(asctime)s %(levelname)s Thread[%(thread)s]: %(message)s")
+        handler.setFormatter(formatter)
         # Uncomment the following lines to output logs in ECS-compatible format
         # formatter = ecs_logging.StdlibFormatter()
         # handler.setFormatter(formatter)
@@ -96,27 +102,28 @@ class BaseCommand:
         :param args: Arguments for the targeted function
         :param iterable_list: List to iterate over and create thread
         """
-        documents = []
-        # If iterable_list is present, then iterate over the list and pass each list element
-        # as an argument to the async function, else iterate over number of threads configured
-        if iterable_list:
-            with ThreadPoolExecutor(max_workers=thread_count) as executor:
-                future_to_path = {
-                    executor.submit(func, *args, list_element): list_element
-                    for list_element in iterable_list
-                }
 
-                for future in as_completed(future_to_path):
-                    try:
-                        documents.extend(future.result())
-                    except Exception as exception:
-                        self.logger.exception(
-                            f"Error while fetching the data from Microsoft Teams. Error {exception}"
-                        )
+        callables = []
+        if iterable_list:
+            for list_element in iterable_list:
+                callables.append(functools.partial(func, *args, list_element))
         else:
-            with ThreadPoolExecutor(max_workers=thread_count) as executor:
-                for _ in range(thread_count):
-                    executor.submit(func)
+            callables.append(func)
+
+        documents = []
+        with ThreadPoolExecutor(max_workers=thread_count) as executor:
+            future_to_path = {
+                executor.submit(list_element): list_element
+                for list_element in callables
+            }
+            for future in as_completed(future_to_path):
+                try:
+                    if future.result():
+                        documents.extend(future.result())
+                except Exception as exception:
+                    self.logger.exception(
+                        f"Error while fetching the data from Microsoft Teams. Error {exception}"
+                    )
         return documents
 
     @cached_property
@@ -128,14 +135,74 @@ class BaseCommand:
         """Get access token for fetching the data
         :param is_acquire_for_client: Flag for fetching the access token
         """
-        msal_access_token = MSALAccessToken(self.logger, self.config)
-        return msal_access_token.get_token(is_acquire_for_client)
+        return MSALAccessToken(self.logger, self.config).get_token(is_acquire_for_client)
 
     def microsoft_calendar_object(self, access_token):
         """Get the object for fetching the calendar related data"""
         return MSTeamsCalendar(
             access_token, get_schema_fields, self.logger, self.config
         )
+
+    def get_mapped_users(self):
+        """Returns mapped users from the CSV file
+        """
+        rows = {}
+        mapping_sheet_path = self.config.get_value("microsoft_teams.user_mapping")
+        if (
+            mapping_sheet_path and os.path.exists(mapping_sheet_path) and os.path.getsize(mapping_sheet_path) > 0
+        ):
+            with open(mapping_sheet_path, encoding="UTF-8") as file:
+                for row in csv.reader(file):
+                    rows[row[0]] = row[1]
+        return rows
+
+    def manage_permissions(self, object_permissions, ws_user, ws_permissions):
+        """Returns the permissions differs from Workplace Search
+        :param object_permissions: Permissions of the Microsoft Teams Object
+        :param ws_user: Workplace Search user
+        :param ws_permissions: Workplace Search permissions of a user
+        """
+        mapped_users = self.get_mapped_users()
+        for ms_team_user, permissions in object_permissions.items():
+            ms_team_user = mapped_users.get(ms_team_user, ms_team_user)
+            if ms_team_user.lower() == ws_user.lower():
+                ws_permissions = set(ws_permissions).difference(permissions)
+        return list(ws_permissions)
+
+    def remove_object_permissions(self, start_time, end_time):
+        """Remove the permissions of the users removed from the Microsoft Teams objects
+        :param start_time: Start time to fetch the permissions
+        :param end_time: End time to fetch the permissions
+        """
+        deleted_permissions_list = []
+        enterprise_search_source_id = self.config.get_value("enterprise_search.source_id")
+
+        microsoft_teams_object = self.microsoft_team_channel_object(
+            self.get_access_token()
+        )
+
+        teams_permissions = microsoft_teams_object.get_team_members()
+
+        ws_user_permissions = PermissionSyncCommand(
+            self.logger, self.config, self.workplace_search_client
+        ).list_user_permissions()
+
+        for ws_user, ws_permissions in ws_user_permissions.items():
+            ws_permissions = self.manage_permissions(teams_permissions, ws_user, ws_permissions)
+            deleted_permissions_list.append({"user": ws_user, "permissions": ws_permissions})
+
+        for permission_dict in deleted_permissions_list:
+            if permission_dict['permissions']:
+                self.workplace_search_client.remove_user_permissions(
+                    content_source_id=enterprise_search_source_id,
+                    user=permission_dict['user'],
+                    body={
+                        "permissions": permission_dict['permissions']
+                    }
+                )
+                self.logger.debug(f"Removed permissions for {permission_dict['user']} from the Workplace Search")
+            else:
+                self.logger.debug(f"No permission found for {permission_dict['user']} to remove from Workplace Search")
 
     def create_jobs_for_calendars(
         self, indexing_type, sync_microsoft_teams, start_time, end_time, queue
@@ -155,18 +222,13 @@ class BaseCommand:
         storage_with_collection = self.local_storage.get_storage_with_collection(
             constant.CALENDAR_CHAT_DELETION_PATH
         )
-        ids_list = storage_with_collection.get("global_keys")
+        ids_list = storage_with_collection.get("global_keys", [])
         try:
             calendar_object = self.microsoft_calendar_object(
                 self.get_access_token(is_acquire_for_client=True)
             )
-            (
-                calendar_permissions,
-                calendar_documents,
-            ) = sync_microsoft_teams.fetch_calendars(
-                calendar_object, ids_list, start_time, end_time
-            )
-            queue.append_to_queue(constant.CALENDAR, calendar_documents)
+            calendar_permissions = sync_microsoft_teams.fetch_calendars(
+                calendar_object, ids_list, start_time, end_time, False)
 
             if self.config.get_value("enable_document_permission"):
                 sync_microsoft_teams.sync_permissions(calendar_permissions)
