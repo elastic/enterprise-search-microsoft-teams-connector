8aabc826067430969fa8c7fbf342a6046b3ba690
moxarth-elastic <moxarth.rathod@elastic.co>
2022-05-30 18:00:20 +0530

    Add base command file

--- ees_microsoft_teams/base_command.py
+++ ees_microsoft_teams/base_command.py
@@ -10,16 +10,27 @@
     objects and methods that will can be used by commands.
 """
 
+import csv
+import functools
 import logging
+import os
 
 try:
     from functools import cached_property
 except ImportError:
     from cached_property import cached_property
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 from elastic_enterprise_search import WorkplaceSearch
 
+from . import constant
 from .configuration import Configuration
+from .local_storage import LocalStorage
+from .microsoft_teams_channels import MSTeamsChannels
+from .msal_access_token import MSALAccessToken
+from .permission_sync_command import PermissionSyncCommand
+from .utils import get_schema_fields, split_documents_into_equal_chunks
 
 
 class BaseCommand:
@@ -48,6 +59,8 @@ class BaseCommand:
         logger.setLevel(log_level)
 
         handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(asctime)s %(levelname)s Thread[%(thread)s]: %(message)s")
+        handler.setFormatter(formatter)
         # Uncomment the following lines to output logs in ECS-compatible format
         # formatter = ecs_logging.StdlibFormatter()
         # handler.setFormatter(formatter)
@@ -80,3 +93,233 @@ class BaseCommand:
         """Get the configuration for the connector for the running command."""
         file_name = self.args.config_file
         return Configuration(file_name)
+
+    def create_jobs(self, thread_count, func, args, iterable_list):
+        """Creates a thread pool of given number of thread count
+        :param thread_count: Total number of threads to be spawned
+        :param func: The target function on which the async calls would be made
+        :param args: Arguments for the targeted function
+        :param iterable_list: List to iterate over and create thread
+        """
+
+        callables = []
+        if iterable_list:
+            for list_element in iterable_list:
+                callables.append(functools.partial(func, *args, list_element))
+        else:
+            callables.append(func)
+
+        documents = []
+        with ThreadPoolExecutor(max_workers=thread_count) as executor:
+            future_to_path = {
+                executor.submit(list_element): list_element
+                for list_element in callables
+            }
+            for future in as_completed(future_to_path):
+                try:
+                    if future.result():
+                        documents.extend(future.result())
+                except Exception as exception:
+                    self.logger.exception(
+                        f"Error while fetching the data from Microsoft Teams. Error {exception}"
+                    )
+        return documents
+
+    @cached_property
+    def local_storage(self):
+        """Get the object for local storage to fetch and update ids stored locally"""
+        return LocalStorage(self.logger)
+
+    def get_access_token(self, is_acquire_for_client=False):
+        """Get access token for fetching the data
+        :param is_acquire_for_client: Flag for fetching the access token
+        """
+        return MSALAccessToken(self.logger, self.config).get_token(is_acquire_for_client)
+
+    def microsoft_team_channel_object(self, access_token):
+        """Get the object for fetching the teams and its children"""
+        return MSTeamsChannels(
+            access_token, get_schema_fields, self.logger, self.config
+        )
+
+    def get_mapped_users(self):
+        """Returns mapped users from the CSV file
+        """
+        rows = {}
+        mapping_sheet_path = self.config.get_value("microsoft_teams.user_mapping")
+        if (
+            mapping_sheet_path and os.path.exists(mapping_sheet_path) and os.path.getsize(mapping_sheet_path) > 0
+        ):
+            with open(mapping_sheet_path, encoding="UTF-8") as file:
+                for row in csv.reader(file):
+                    rows[row[0]] = row[1]
+        return rows
+
+    def manage_permissions(self, object_permissions, ws_user, ws_permissions):
+        """Returns the permissions differs from Workplace Search
+        :param object_permissions: Permissions of the Microsoft Teams Object
+        :param ws_user: Workplace Search user
+        :param ws_permissions: Workplace Search permissions of a user
+        """
+        mapped_users = self.get_mapped_users()
+        for ms_team_user, permissions in object_permissions.items():
+            ms_team_user = mapped_users.get(ms_team_user, ms_team_user)
+            if ms_team_user.lower() == ws_user.lower():
+                ws_permissions = set(ws_permissions).difference(permissions)
+        return list(ws_permissions)
+
+    def remove_object_permissions(self, start_time, end_time):
+        """Remove the permissions of the users removed from the Microsoft Teams objects
+        :param start_time: Start time to fetch the permissions
+        :param end_time: End time to fetch the permissions
+        """
+        deleted_permissions_list = []
+        enterprise_search_source_id = self.config.get_value("enterprise_search.source_id")
+
+        microsoft_teams_object = self.microsoft_team_channel_object(
+            self.get_access_token()
+        )
+
+        teams_permissions = microsoft_teams_object.get_team_members()
+
+        ws_user_permissions = PermissionSyncCommand(
+            self.logger, self.config, self.workplace_search_client
+        ).list_user_permissions()
+
+        for ws_user, ws_permissions in ws_user_permissions.items():
+            ws_permissions = self.manage_permissions(teams_permissions, ws_user, ws_permissions)
+            deleted_permissions_list.append({"user": ws_user, "permissions": ws_permissions})
+
+        for permission_dict in deleted_permissions_list:
+            if permission_dict['permissions']:
+                self.workplace_search_client.remove_user_permissions(
+                    content_source_id=enterprise_search_source_id,
+                    user=permission_dict['user'],
+                    body={
+                        "permissions": permission_dict['permissions']
+                    }
+                )
+                self.logger.debug(f"Removed permissions for {permission_dict['user']} from the Workplace Search")
+            else:
+                self.logger.debug(f"No permission found for {permission_dict['user']} to remove from Workplace Search")
+
+    def create_jobs_for_teams(
+        self,
+        indexing_type,
+        sync_microsoft_teams,
+        thread_count,
+        start_time,
+        end_time,
+        queue,
+    ):
+        """Creates jobs for fetching the teams and its children objects
+        :param indexing_type: The type of the indexing i.e. Full or Incremental
+        :param sync_microsoft_teams: Object for fetching the Microsoft Teams object
+        :param thread_count: Thread count to make partitions
+        :param start_time: Start time for fetching the data
+        :param end_time: End time for fetching the data
+        :param queue: Shared queue for storing the data
+        """
+        allowed_objects = [
+            "teams",
+            "channels",
+            "channel_messages",
+            "channel_tabs",
+            "channel_documents",
+        ]
+        if not any(teams_object in self.config.get_value("objects") for teams_object in allowed_objects):
+            return
+
+        storage_with_collection = self.local_storage.get_storage_with_collection(
+            constant.CHANNEL_CHAT_DELETION_PATH
+        )
+        ids_list = storage_with_collection.get("global_keys")
+
+        self.logger.debug("Started fetching the teams and its objects data...")
+        microsoft_teams_object = self.microsoft_team_channel_object(
+            self.get_access_token()
+        )
+        try:
+            if self.config.get_value("enable_document_permission"):
+                user_permissions = microsoft_teams_object.get_team_members()
+                sync_microsoft_teams.sync_permissions(user_permissions)
+
+            teams = sync_microsoft_teams.fetch_teams(microsoft_teams_object, ids_list, False)
+
+            configuration_objects = self.config.get_value("objects")
+
+            teams_partition_list = split_documents_into_equal_chunks(
+                teams, thread_count
+            )
+
+            channels = self.create_jobs(
+                thread_count,
+                sync_microsoft_teams.fetch_channels,
+                (
+                    microsoft_teams_object,
+                    ids_list,
+                    False
+                ),
+                teams_partition_list,
+            )
+
+            channels_partition_list = split_documents_into_equal_chunks(
+                channels, thread_count
+            )
+
+            if "channel_messages" in configuration_objects:
+                self.create_jobs(
+                    thread_count,
+                    sync_microsoft_teams.fetch_channel_messages,
+                    (
+                        microsoft_teams_object,
+                        start_time,
+                        end_time,
+                        ids_list,
+                        False
+                    ),
+                    channels_partition_list,
+                )
+
+            if "channel_tabs" in configuration_objects:
+                self.create_jobs(
+                    thread_count,
+                    sync_microsoft_teams.fetch_channel_tabs,
+                    (
+                        microsoft_teams_object,
+                        start_time,
+                        end_time,
+                        ids_list,
+                        False
+                    ),
+                    channels_partition_list,
+                )
+
+            if "channel_documents" in configuration_objects:
+                self.create_jobs(
+                    thread_count,
+                    sync_microsoft_teams.fetch_channel_documents,
+                    (
+                        microsoft_teams_object,
+                        start_time,
+                        end_time,
+                        ids_list,
+                        False
+                    ),
+                    teams_partition_list,
+                )
+
+            storage_with_collection["global_keys"] = list(ids_list)
+            self.local_storage.update_storage(
+                storage_with_collection, constant.CHANNEL_CHAT_DELETION_PATH
+            )
+
+            self.logger.debug("Saving the checkpoint for Teams and its objects")
+            queue.put_checkpoint("teams", end_time, indexing_type)
+        except Exception as exception:
+            self.logger.exception(
+                f"Error while fetching the teams or it's objects data. Error: {exception}"
+            )
+        self.logger.info(
+            "Completed fetching of teams and it's objects data to the Workplace Search"
+        )
