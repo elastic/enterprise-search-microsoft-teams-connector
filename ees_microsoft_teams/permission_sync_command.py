978fc2ede1aca2a91c5aed7cade4181ca853a974
moxarth-elastic <moxarth.rathod@elastic.co>
2022-06-01 23:30:20 +0530

    update the base files

--- ees_microsoft_teams/permission_sync_command.py
+++ ees_microsoft_teams/permission_sync_command.py
@@ -10,9 +10,9 @@
 class PermissionSyncCommand:
     """This class contains logic to sync user permissions from Microsoft Teams."""
 
-    def __init__(self, logger, config, workplace_search_custom_client):
+    def __init__(self, logger, config, workplace_search_client):
         self.logger = logger
-        self.workplace_search_custom_client = workplace_search_custom_client
+        self.workplace_search_client = workplace_search_client
         self.config = config
         self.enterprise_search_source_id = self.config.get_value("enterprise_search.source_id")
 
@@ -20,14 +20,10 @@ class PermissionSyncCommand:
         """ Returns the list of users permissions from Workplace Search
         """
         self.logger.debug("Fetching the user permissions from the workplace search")
+        user_permission = self.workplace_search_client.list_permissions(
+            content_source_id=self.enterprise_search_source_id,
+        )
         user_permissions_dict = {}
-
-        user_permission = self.workplace_search_custom_client.list_permissions()
-        for user_dict in user_permission["results"]:
-            user_name = user_dict['user'] if 'user' in user_dict else user_dict["external_user_properties"][0][
-                "attribute_value"]
-            user_permissions_dict.update(
-                {user_name: user_dict["permissions"]}
-            )
-        self.logger.info("Successfully retrieved all permissions from the Workplace Search")
+        for user_dict in user_permission['results']:
+            user_permissions_dict.update({user_dict["user"]: user_dict["permissions"]})
         return user_permissions_dict
