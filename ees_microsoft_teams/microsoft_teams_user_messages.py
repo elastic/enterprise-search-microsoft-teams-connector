bf5507b403e354f35aa7c477a46c29a732817319
root <root@localhost.localdomain>
2022-09-05 16:14:29 +0530

    resolve merge conflicts

--- ees_microsoft_teams/microsoft_teams_user_messages.py
+++ ees_microsoft_teams/microsoft_teams_user_messages.py
@@ -31,6 +31,192 @@ class MSTeamsUserMessage:
         self.object_type_to_index = config.get_value('object_type_to_index')
         self.local_storage = local_storage
 
+    def get_attachments(
+        self,
+        user_id,
+        prefix,
+        attachment_name,
+        attachment_id,
+        chat_id,
+        updated_date,
+        ids_list,
+        user_drive,
+        attachment_client
+    ):
+        """Fetches all the attachments of a user chat
+        :param user_id: Id of the user
+        :param prefix: Title of the chat message
+        :param attachment_name: Name of the attachment
+        :param attachment_id: Id of the attachment
+        :param chat_id: Id of chat
+        :param updated_date: date of chat updated
+        :param ids_list: List of ids
+        :param user_drive: Dictionary of user id with drive id
+        :param attachment_client: Object of Microsoft team client
+        Returns:
+            attachment_list: Documents to be indexed in Workplace Search
+        """
+        try:
+            item_id = None
+
+            # Checking if the user_id is present in user_drive dictionary for saving the else iteration. If user_id is
+            # present in the dictionary then we'll directly use its drive_id for fetching attachments.
+            if user_drive.get(user_id):
+                drive_id = list(user_drive[user_id].keys())[0]
+
+            else:
+                user_drive_response = attachment_client.get_user_chat_attachment_drive(
+                    f"{constant.GRAPH_BASE_URL}/users/{user_id}/drive"
+                )
+
+                if user_drive_response:
+                    # Logic to append user for deletion
+                    self.local_storage.insert_document_into_doc_id_storage(
+                        ids_list, user_id, constant.USER, "", ""
+                    )
+                    drive_id = user_drive_response["id"]
+                    user_drive[user_id] = {drive_id: None}
+
+                    # Logic to append user drive for deletion
+                    self.local_storage.insert_document_into_doc_id_storage(
+                        ids_list, drive_id, constant.USER_CHAT_DRIVE, user_id, ""
+                    )
+
+            if user_drive:
+                # Check that item_id is present with the drive id or not for saving the else iteration.
+                if user_drive.get(user_id).get(drive_id):
+                    item_id = user_drive.get(user_id).get(drive_id)
+
+                else:
+                    user_root_response_data = attachment_client.get_user_chat_attachment_drive_children(
+                        f"{constant.GRAPH_BASE_URL}/drives/{drive_id}/items/root/children"
+                    )
+
+                    if user_root_response_data:
+                        for child in user_root_response_data:
+                            if child["name"] == "Microsoft Teams Chat Files":
+                                item_id = child["id"]
+                                user_drive[user_id][drive_id] = item_id
+                                break
+
+            attachment_list = []
+            if not item_id:
+                return []
+
+            # Logic to append user drive item for deletion
+            self.local_storage.insert_document_into_doc_id_storage(
+                ids_list, item_id, constant.USER_CHAT_DRIVE_ITEM, drive_id, user_id
+            )
+            final_attachment_url = f"{constant.GRAPH_BASE_URL}/drives/{drive_id}/items/{item_id}/children?" \
+                f"$filter=name eq '{url_encode(attachment_name)}'"
+            attachment_response_data = attachment_client.get_user_chat_attachment_drive_children(
+                final_attachment_url)
+
+            if attachment_response_data:
+                document = attachment_response_data[0]
+                attachment_dict = {"type": USER_CHAT_ATTACHMENT}
+                is_file = document.get("file", {})
+
+                if is_file and type(is_file) != float:
+                    mimetype = is_file.get("mimeType")
+
+                    if mimetype not in constant.MIMETYPES:
+                        attachment_content_response = requests.get(
+                            document.get("@microsoft.graph.downloadUrl")
+                        )
+
+                        if attachment_content_response:
+                            attachment_content = extract_api_response(
+                                attachment_content_response.content
+                            )
+
+                            attachment_dict["id"] = attachment_id
+                            attachment_dict["title"] = f"{prefix}-{attachment_name}"
+                            attachment_dict["body"] = attachment_content or ""
+                            attachment_dict["url"] = document.get("webUrl")
+                            attachment_dict["last_updated"] = updated_date
+                            attachment_dict["_allow_permissions"] = []
+                            if self.is_permission_sync_enabled:
+                                attachment_dict["_allow_permissions"] = [chat_id]
+
+                            attachment_list.append(attachment_dict)
+                            # Logic to append user chat attachment for deletion
+                            self.local_storage.insert_document_into_doc_id_storage(
+                                ids_list,
+                                attachment_id,
+                                USER_CHAT_ATTACHMENT,
+                                item_id,
+                                drive_id,
+                            )
+            return attachment_list
+        except Exception as exception:
+            self.logger.exception(
+                f"[Fail] Error while fetching attachments for the user chats. Error: {exception}"
+            )
+
+    def fetch_tabs(self, chat_id, ids_list, start_time, end_time):
+        """Fetches user chat tabs from the Microsoft Teams
+        :param chat_id: Id of the chat
+        :param ids_list: List of ids
+        :param start_time: Starting time for fetching data
+        :param end_time: Ending time for fetching data
+        Returns:
+            documents: Documents to be indexed in Workplace Search
+        """
+        try:
+            documents = []
+            tab_detail_response = self.client.get_user_chat_tabs(
+                f"{constant.GRAPH_BASE_URL}/chats/{chat_id}/tabs",
+                start_time, end_time, chat_id
+            )
+
+            if tab_detail_response:
+                tab_schema = get_schema_fields("user_tabs", self.object_type_to_index)
+                for tab in tab_detail_response:
+                    tab_dict = {"type": USER_CHAT_TABS}
+                    for ws_field, ms_fields in tab_schema.items():
+                        tab_dict[ws_field] = tab[ms_fields]
+                    tab_dict["url"] = tab["configuration"]["websiteUrl"]
+
+                    tab_dict["_allow_permissions"] = []
+                    if self.is_permission_sync_enabled:
+                        tab_dict["_allow_permissions"] = [chat_id]
+                    documents.append(tab_dict)
+                    self.local_storage.insert_document_into_doc_id_storage(
+                        ids_list, tab["id"], USER_CHAT_TABS, chat_id, ""
+                    )
+            return documents
+        except Exception as exception:
+            self.logger.exception(
+                f"[Fail] Error while fetching user tabs from teams. Error: {exception}"
+            )
+            raise
+
+    def fetch_meeting_recording(self, chat_id, chat):
+        """Fetches meeting recording from the Microsoft Teams
+        :param chat_id: Id of the chat
+        :param chat: dictionary of the user chat
+        Returns: recording_dict: Document to be indexed in Workplace Search
+        """
+        if (
+            chat["eventDetail"] and chat["eventDetail"][
+                "@odata.type"] == "#microsoft.graph.callRecordingEventMessageDetail"
+        ):
+            url = chat["eventDetail"].get("callRecordingUrl")
+
+            if url and ".sharepoint.com" in url:
+                recording_dict = {"type": MEETING_RECORDING}
+                recording_dict["id"] = chat["eventDetail"]["callId"]
+                recording_dict["title"] = chat["eventDetail"][
+                    "callRecordingDisplayName"
+                ]
+                recording_dict["url"] = url
+
+                recording_dict["_allow_permissions"] = []
+                if self.is_permission_sync_enabled:
+                    recording_dict["_allow_permissions"] = [chat_id]
+                return recording_dict
+
     def get_user_chat_messages(
         self,
         ids_list,
---
+++ tests/test_client.py
@@ -0,0 +1,259 @@
+import logging
+import os
+
+import pytest
+from ees_microsoft_teams.configuration import Configuration
+from ees_microsoft_teams.microsoft_teams_client import MSTeamsClient
+
+CONFIG_FILE = os.path.join(
+    os.path.join(os.path.dirname(__file__), "config"),
+    "microsoft_teams_connector.yml",
+)
+
+
+def settings():
+    """This function loads configuration from the file and returns it along with retry_count setting.
+    :param requests_mock: fixture for requests.get calls.
+    """
+    configuration = Configuration(file_name=CONFIG_FILE)
+    logger = logging.getLogger("unit_test_full_sync_command")
+    return configuration, logger
+
+
+def create_client_obj():
+    """This function create client object for test.
+    """
+    configs, logger = settings()
+    return MSTeamsClient(logger, 'token', configs)
+
+
+@pytest.mark.parametrize(
+    "mock_teams, source_teams",
+    [
+        (
+            {
+                "value": [
+                    {
+                        "id": "45b7d2e7-b882-4a80-ba97-10b7a63b8fa4",
+                        "createdDateTime": "2018-12-22T02:21:05Z",
+                        "description": "Self help community for golf",
+                        "displayName": "Golf Assist",
+                        "mail": "golfassist@contoso.com",
+                        "mailNickname": "golfassist",
+                        "renewedDateTime": "2018-12-22T02:21:05Z",
+                    },
+                ]
+            },
+            [
+                {
+                    "id": "45b7d2e7-b882-4a80-ba97-10b7a63b8fa4",
+                    "createdDateTime": "2018-12-22T02:21:05Z",
+                    "description": "Self help community for golf",
+                    "displayName": "Golf Assist",
+                    "mail": "golfassist@contoso.com",
+                    "mailNickname": "golfassist",
+                    "renewedDateTime": "2018-12-22T02:21:05Z",
+                },
+            ]
+        )
+    ],
+)
+def test_get_teams(mock_teams, source_teams, requests_mock):
+    """ Test get_teams method of client file
+    """
+    # Setup
+    _, _ = settings()
+    client_obj = create_client_obj()
+    headers = {
+        "authorization": "Bearer dummy_access_token",
+        "content-type": "application/json",
+    }
+    requests_mock.get(
+        "https://graph.microsoft.com/v1.0/groups?$top=999",
+        headers=headers,
+        json=mock_teams,
+        status_code=200,
+    )
+
+    # Execute
+    target_teams = client_obj.get_teams("https://graph.microsoft.com/v1.0/groups")
+
+    # Assert
+    assert source_teams == target_teams
+
+
+@pytest.mark.parametrize(
+    "mock_channels, source_channels",
+    [
+        (
+            {
+                "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#teams('441b02e8-3dd8-4640-9d8b-bb0b6f2ce439')/channels",
+                "@odata.count": 16,
+                "value": [
+                    {
+                        "id": "19:45f123d269a94101bebb60b7678a45c2@thread.tacv2",
+                        "createdDateTime": "2022-06-29T06:25:09.386Z",
+                        "displayName": "Test Channel",
+                        "description": "Channel for testing purpose",
+                        "tenantId": "5186e740-5d4d-452f-b9c1-c8131cdefbe9",
+                        "webUrl": "https://teams.microsoft.com/l/channel/19%3A45f123d269a94101bebb60b7678a45c2%40thread.tacv2/TestTeam-3_Channel-1?groupId=441b02e8-3dd8-4640-9d8b-bb0b6f2ce439&tenantId=5186e740-5d4d-452f-b9c1-c8131cdefbe9&allowXTenantAccess=False",
+                        "membershipType": "standard"
+                    },
+                ]
+            },
+            [
+                {
+                    "id": "19:45f123d269a94101bebb60b7678a45c2@thread.tacv2",
+                    "createdDateTime": "2022-06-29T06:25:09.386Z",
+                    "displayName": "Test Channel",
+                    "description": "Channel for testing purpose",
+                    "tenantId": "5186e740-5d4d-452f-b9c1-c8131cdefbe9",
+                    "webUrl": "https://teams.microsoft.com/l/channel/19%3A45f123d269a94101bebb60b7678a45c2%40thread.tacv2/TestTeam-3_Channel-1?groupId=441b02e8-3dd8-4640-9d8b-bb0b6f2ce439&tenantId=5186e740-5d4d-452f-b9c1-c8131cdefbe9&allowXTenantAccess=False",
+                    "membershipType": "standard"
+                },
+            ]
+        )
+    ],
+)
+def test_get_channels(mock_channels, source_channels, requests_mock):
+    """ test get_channels method of client file
+    """
+    # Setup
+    _, _ = settings()
+    client_obj = create_client_obj()
+    headers = {
+        "authorization": "Bearer dummy_access_token",
+        "content-type": "application/json",
+    }
+    requests_mock.get(
+        "https://graph.microsoft.com/v1.0/teams/1/channels",
+        headers=headers,
+        json=mock_channels,
+        status_code=200,
+    )
+
+    # Execute
+    target_channels = client_obj.get_teams("https://graph.microsoft.com/v1.0/teams/1/channels")
+
+    # Assert
+    assert source_channels == target_channels
+
+
+@pytest.mark.parametrize(
+    "mock_channel_messages, source_channel_messages",
+    [
+        (
+            {
+                "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#teams('team1')/channels('team1_channel1')/messages",
+                "@odata.count": 16,
+                "value": [
+                    {
+                        "id": "1661755405876",
+                        "replyToId": None,
+                        "etag": "1661755405876",
+                        "messageType": "message",
+                        "createdDateTime": "2022-08-29T06:43:25.876Z",
+                        "lastModifiedDateTime": "2022-08-29T06:43:25.876Z",
+                        "lastEditedDateTime": None,
+                        "deletedDateTime": None,
+                        "subject": None,
+                        "summary": None,
+                        "chatId": None,
+                        "importance": "normal",
+                        "locale": "en-us",
+                        "webUrl": "https://teams.microsoft.com/l/message/19%3Ajy3xYhYibQjuEqyNebuqYuG48vGfhjigtMqzjoq7Kfs1%40thread.tacv2/1661755405876?groupId=feffa8d9-33b9-42ac-87d6-b6a512472e27&tenantId=5186e740-5d4d-452f-b9c1-c8131cdefbe9&createdTime=1661755405876&parentMessageId=1661755405876",
+                        "policyViolation": None,
+                        "eventDetail": None,
+                        "from": {
+                            "application": None,
+                            "device": None,
+                            "user": {
+                                "id": "1cd554ab-469a-4d74-93c7-33a05ea12342",
+                                "displayName": "moxarth rathod",
+                                "userIdentityType": "aadUser"
+                            }
+                        },
+                        "body": {
+                            "contentType": "text",
+                            "content": "New Message for testing"
+                        },
+                        "channelIdentity": {
+                            "teamId": "feffa8d9-33b9-42ac-87d6-b6a512472e27",
+                            "channelId": "19:jy3xYhYibQjuEqyNebuqYuG48vGfhjigtMqzjoq7Kfs1@thread.tacv2"
+                        },
+                        "attachments": [],
+                        "mentions": [],
+                        "reactions": []
+                    },
+                ]
+            },
+            [
+                {
+                        "id": "1661755405876",
+                        "replyToId": None,
+                        "etag": "1661755405876",
+                        "messageType": "message",
+                        "createdDateTime": "2022-08-29T06:43:25.876Z",
+                        "lastModifiedDateTime": "2022-08-29T06:43:25.876Z",
+                        "lastEditedDateTime": None,
+                        "deletedDateTime": None,
+                        "subject": None,
+                        "summary": None,
+                        "chatId": None,
+                        "importance": "normal",
+                        "locale": "en-us",
+                        "webUrl": "https://teams.microsoft.com/l/message/19%3Ajy3xYhYibQjuEqyNebuqYuG48vGfhjigtMqzjoq7Kfs1%40thread.tacv2/1661755405876?groupId=feffa8d9-33b9-42ac-87d6-b6a512472e27&tenantId=5186e740-5d4d-452f-b9c1-c8131cdefbe9&createdTime=1661755405876&parentMessageId=1661755405876",
+                        "policyViolation": None,
+                        "eventDetail": None,
+                        "from": {
+                            "application": None,
+                            "device": None,
+                            "user": {
+                                "id": "1cd554ab-469a-4d74-93c7-33a05ea12342",
+                                "displayName": "moxarth rathod",
+                                "userIdentityType": "aadUser"
+                            }
+                        },
+                        "body": {
+                            "contentType": "text",
+                            "content": "New Message for testing"
+                        },
+                        "channelIdentity": {
+                            "teamId": "feffa8d9-33b9-42ac-87d6-b6a512472e27",
+                            "channelId": "19:jy3xYhYibQjuEqyNebuqYuG48vGfhjigtMqzjoq7Kfs1@thread.tacv2"
+                        },
+                        "attachments": [],
+                        "mentions": [],
+                        "reactions": []
+                    },
+            ]
+        )
+    ],
+)
+def test_get_channel_messages(mock_channel_messages, source_channel_messages, requests_mock):
+    """ test get_channels method of client file
+    """
+    # Setup
+    _, _ = settings()
+    client_obj = create_client_obj()
+    headers = {
+        "authorization": "Bearer dummy_access_token",
+        "content-type": "application/json",
+    }
+    requests_mock.get(
+        "https://graph.microsoft.com/v1.0/teams/team1/channels/team1_channel1/messages",
+        headers=headers,
+        json=mock_channel_messages,
+        status_code=200,
+    )
+
+    # Execute
+    target_channel_messages = client_obj.get_channel_messages(
+        next_url="https://graph.microsoft.com/v1.0/teams/team1/channels/team1_channel1/messages",
+        start_time="2022-08-28T06:43:25.876Z",
+        end_time="2022-08-30T06:43:25.876Z",
+        channel_name="team1_channel1"
+    )
+
+    # Assert
+    assert source_channel_messages == target_channel_messages
