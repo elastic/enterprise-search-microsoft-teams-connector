9b4952aaac25002a9c260a64e336cbc6a7f3326b
Artem Shelkovnikov <artem.shelkovnikov@elastic.co>
2022-10-11 20:05:53 +0200

    Update msal_access_token.py
    Fixing license header

--- ees_microsoft_teams/msal_access_token.py
+++ ees_microsoft_teams/msal_access_token.py
@@ -1,8 +1,7 @@
 #
 # Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-# or more contributor license agreements. Licensed under the Elastic License
-# 2.0; you may not use this file except in compliance with the Elastic License
-# 2.0.
+# or more contributor license agreements. Licensed under the Elastic License 2.0;
+# you may not use this file except in compliance with the Elastic License 2.0.
 #
 """ This module is used to generate the access token required to authenticate the
     Microsoft Graph APIs.
